5b8120801c2277f1efad01286f042fc138e4d8b7
小林優一 <y.k94cofitec179@gmail.com>
2022-05-17 20:04:26 +0900

    Google認証機能追加

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,14 +4,15 @@ import "firebase/firestore";
 import "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "process.env.REACT_APP_FIREBASE_APIKEY",
-  authDomain: "process.env.REACT_APP_FIREBASE_DOMAIN",
-  databaseURL: "process.env.REACT_APP_FIREBASE_DATABASE",
-  projectId: "process.env.REACT_APP_FIREBASE_PROJECT_ID",
-  storageBucket: "process.env.REACT_APP_FIREBASE_STORAGE_BUCKET",
-  messagingSenderId: "process.env.REACT_APP_FIREBASE_SENDER_ID",
-  appId: "process.env.REACT_APP_FIREBASE_APP_ID",
+  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
+
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
 export const db = firebaseApp.firestore();
